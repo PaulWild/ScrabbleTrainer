a3434d3746f2740414acef52d4b6d3a8ae49c10a
Paul Wild <paulwild@gmail.com>
2020-06-16 03:27:21 +0100

    add values

--- src/components/Tile.tsx
+++ src/components/Tile.tsx
@@ -3,7 +3,37 @@ import React, { memo } from "react";
 import './Tile.css';
 
 export type ScrabbleLetter = "A" | "B" | "C" | "D" | "E" | "F" | "G" | "H" | "I" | "J" | "K" | "L" | "M" | "N" | "O" | "P" | "Q" | "R" | "S" | "T" | "U" | "V" | "W" | "X" | "Y" | "Z" | " ";
-export type ScrablePoint = 0 |1 | 2 | 3 | 4 | 5 | 8 | 10;
+export type ScrablePoint = "" |1 | 2 | 3 | 4 | 5 | 8 | 10;
+
+const lookup = {
+    "A": 1, 
+    "B": 3,
+    "C": 3,
+    "D": 2,
+    "E": 1,
+    "F": 4,
+    "G": 2,
+    "H" : 4,
+    "I" : 1, 
+    "J" : 8, 
+    "K" : 5, 
+    "L" : 1, 
+    "M" : 3, 
+    "N" : 1, 
+    "O" : 1, 
+    "P" : 3, 
+    "Q" : 10, 
+    "R" : 1, 
+    "S" : 1, 
+    "T" : 1, 
+    "U" : 1, 
+    "V" : 4, 
+    "W" : 4, 
+    "X" : 8, 
+    "Y" : 4,
+    "Z" : 10, 
+    " " : ""
+}
 
 interface TileProps {
     letter: ScrabbleLetter
@@ -11,7 +41,7 @@ interface TileProps {
 }
 
 const Tile = ({letter, selected}: TileProps) => {
-    return <div className={`Tile ${selected ? "Selected" : "UnSelected"}`}>{ letter }</div>
+    return <div className={`Tile ${selected ? "Selected" : "UnSelected"}`}>{ letter}<div className="Points">{lookup[letter]}</div> </div>
 }
 
 export default memo(Tile)
