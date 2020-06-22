d36f0f1419f465224db0e3d962cb1c85abbc85f8
Paul Wild <paulwild@gmail.com>
2020-06-22 03:16:53 +0100

    whoops. caching the hell out of everything makes it hard

--- src/dictionaries/dictionaryProvider.tsx
+++ src/dictionaries/dictionaryProvider.tsx
@@ -7,8 +7,8 @@ type WordListState = { state: "Loading"} | { state: "Loaded", words: Set<string>
 
 const Dictionary = (type:string) => {
     return async (firstletter: ScrabbleLetter, length:number): Promise<string[]>  => {
-        //fetch(`https://scrabble.paulwild.dev/api/wordlists?letter=${firstletter}&length=${length}&dict=${type}`)
-        const x = await fetch(`http://localhost:7071/api/wordlists?letter=${firstletter}&length=${length}&dict=${type}`);
+        const x = await fetch(`https://scrabble.paulwild.dev/api/wordlists?letter=${firstletter}&length=${length}&dict=${type}`)
+        //const x = await fetch(`http://localhost:7071/api/wordlists?letter=${firstletter}&length=${length}&dict=${type}`);
         return await x.json();
     }
 }
