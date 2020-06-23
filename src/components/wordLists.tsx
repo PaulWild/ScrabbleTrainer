adaadeda1c37baa573c965784a1d1afb4d3e11e4
Paul Wild <paulwild@gmail.com>
2020-06-23 11:07:45 +0100

    nomnom

--- src/components/wordLists.tsx
+++ src/components/wordLists.tsx
@@ -38,7 +38,7 @@ const useStyles = makeStyles((theme) => ({
   },
     formControl: {
       margin: theme.spacing(1),
-      minWidth: 120,
+      width: '6em',
     },
     cardHeader: {
         display: 'flex',
