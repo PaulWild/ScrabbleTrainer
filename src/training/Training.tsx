f819afcf36325d4d824551df2c65d5331435b110
Paul Wild <paulwild@gmail.com>
2020-06-28 20:31:53 +0100

    fix up training splash

--- src/training/Training.tsx
+++ src/training/Training.tsx
@@ -1,14 +1,15 @@
 import React from 'react'
-import { makeStyles, Typography, Container } from '@material-ui/core'
+import { makeStyles, Typography, Container, IconButton } from '@material-ui/core'
 import {  Routes } from '../App';
 import { useHistory } from 'react-router-dom';
 import ScrabbleCard from '../components/scrabbleCard';
 import LetterGrid from '../components/letterGrid';
+import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     display: "grid",
-    gridTemplateColumns: "repeat(auto-fill, minmax(30em, 1fr));",
+    gridTemplateColumns: "repeat(auto-fill, minmax(15em, 1fr));",
     margin: "1em",
   },
   }))
@@ -27,5 +28,12 @@ export const Training = () => {
       subheader={"Test your knowledge of valid three letter words"}
       avatar = {<Typography variant='h4' component='h5' color='inherit'>3</Typography>}
     content = {<LetterGrid onClick={(l) => () => history.push(Routes.ThreeLetterWords(l))} />}  />
+      <ScrabbleCard title={"Bingo Stems"}
+      subheader={"Test your bingo stem skills"}
+      avatar = {<Typography variant='h4' component='h5' color='inherit'>S</Typography>}
+      action = { <IconButton aria-label="settings" onClick={() => history.push(Routes.StemList)}> 
+      <ArrowForwardIcon />
+      </IconButton> }
+ />
       </Container>)
 }
