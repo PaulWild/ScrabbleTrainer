7858985ceb4d57bfd13bcd08c21997b1cc3474e0
Paul Wild <paulwild@gmail.com>
2020-07-01 01:15:01 +0100

    word check

--- src/training/Training.tsx
+++ src/training/Training.tsx
@@ -8,9 +8,15 @@ import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    display: "grid",
-    gridTemplateColumns: "repeat(auto-fill, minmax(15em, 1fr));",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "left",
+    flexWrap: "wrap",
+    margin: 0
   },
+  item: {
+    flexBasis: "25em"
+  }
   }))
 
 export const Training = () => {
@@ -19,15 +25,15 @@ export const Training = () => {
 
     return (
       <Container className={classes.root}>
-      <ScrabbleCard title={"Two Letter Words"}
+      <ScrabbleCard className={classes.item} title={"Two Letter Words"}
       subheader={"Test your knowledge of valid two letter words"}
       avatar = {<Typography variant='h4' component='h5' color='inherit'>2</Typography>}
       content = {<LetterGrid onClick={(l) => () => history.push(Routes.TwoLetterWords(l))} />}/>
-            <ScrabbleCard title={"Three Letter Words"}
+            <ScrabbleCard title={"Three Letter Words"} className={classes.item}
       subheader={"Test your knowledge of valid three letter words"}
       avatar = {<Typography variant='h4' component='h5' color='inherit'>3</Typography>}
     content = {<LetterGrid onClick={(l) => () => history.push(Routes.ThreeLetterWords(l))} />}  />
-      <ScrabbleCard title={"Bingo Stems"}
+      <ScrabbleCard className={classes.item} title={"Bingo Stems"}
       subheader={"Test your bingo stem skills"}
       avatar = {<Typography variant='h4' component='h5' color='inherit'>S</Typography>}
       action = { <IconButton aria-label="settings" onClick={() => history.push(Routes.StemList)}> 
